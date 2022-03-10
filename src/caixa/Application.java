3e3d53cdffecb40b572d0c80044b8a4910e74bf1
alunoESGTS <alunoESGTS@st_11.ESG.IPSantarem.local>
2022-03-10 17:46:57 +0000

    Metodos

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -5,25 +5,28 @@ public class Application {
 	public static void main(String[] args) {
 		
 		//TOPO
+		
+		
+		//Lateral
+		vertical();
+		
+		//Fim
+		horizontal();
+
+	
+	}
+	
+	private static void horizontal() {
 		for(int i = 0; i < 10; i++) {
 			System.out.print("0");
 		}
-
-		
-		//Lateral
+		System.out.println("");
+	}
+	private static void vertical() {
 		for(int i = 0; i < 10; i++) {
 			System.out.print("0");
 			System.out.print("         ");
 			System.out.println("0");
 		}
-
-		
-		//Fim
-		for(int i = 0; i < 11; i++) {
-			System.out.print("0");
-		}
-
-	
 	}
-
 }
