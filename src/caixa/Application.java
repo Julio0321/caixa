3087c8dc81c3f96933e61ef8447cdc1eb303bc8c
alunoESGTS <alunoESGTS@st_11.ESG.IPSantarem.local>
2022-03-10 17:35:36 +0000

    Src

---
+++ src/caixa/Application.java
@@ -0,0 +1,29 @@
+package caixa;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		//TOPO
+		for(int i = 0; i < 10; i++) {
+			System.out.print("0");
+		}
+
+		
+		//Lateral
+		for(int i = 0; i < 10; i++) {
+			System.out.print("0");
+			System.out.print("         ");
+			System.out.println("0");
+		}
+
+		
+		//Fim
+		for(int i = 0; i < 11; i++) {
+			System.out.print("0");
+		}
+
+	
+	}
+
+}
