7a38268275ff65a3bdd49e0fabba6222a2b15576
alunoESGTS <alunoESGTS@st_11.ESG.IPSantarem.local>
2022-03-17 17:28:42 +0000

    a

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -4,10 +4,16 @@ public class Application {
 
 	public static void main(String[] args) {
 		
+<<<<<<< HEAD
 		Caixa cx = new Caixa(5,5, ".");
 		
 		cx.imprimir();
 		cx.setCaracter("/");
 		cx.imprimir();
+=======
+		Caixa cx = new Caixa(10,10);
+		
+		cx.imprimir();
+>>>>>>> origin/master
 	}
 }
--- src/caixa/Caixa.java
+++ src/caixa/Caixa.java
@@ -2,15 +2,21 @@ package caixa;
 
 public class Caixa {
 	
+<<<<<<< HEAD
 	private final int linhas;
 	private final int colunas;
 	private  String simbol;
 
+=======
+	private int linhas;
+	private int colunas;
+>>>>>>> origin/master
 	
 //	int linhas = 5;
 //	int colunas = 5;
 
 	
+<<<<<<< HEAD
 	public Caixa(int linhas,int colunas,String simbol) {
 		this.linhas = linhas;
 		this.colunas = colunas;
@@ -19,10 +25,16 @@ public class Caixa {
 	
 	public void setCaracter(String simbol) {
 		this.simbol = simbol;	
+=======
+	public Caixa(int linhas,int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+>>>>>>> origin/master
 	}
 	
 	public void imprimir() {
 		//TOPO
+<<<<<<< HEAD
 		System.out.print(horizontal());
 		
 		//Lateral
@@ -59,4 +71,31 @@ public class Caixa {
 		return resultado1;
     }
 	
+=======
+		horizontal(colunas);
+		
+		//Lateral
+		vertical(linhas,colunas);
+		
+		//Fim
+		horizontal(colunas);
+	}
+	
+	private static void horizontal(int colunas) {
+		for(int i = 0; i < colunas; i++) {
+			System.out.print("0");
+		}
+		System.out.println("");
+	}
+	
+	private static void vertical(int linhas,int colunas) {
+		for(int i = 0; i < linhas; i++) {
+			System.out.print("0");
+			for(int x = 0; x < colunas - 2 ; x++) {
+			System.out.print(" ");
+			}
+			System.out.println("0");
+		}
+    }
+>>>>>>> origin/master
 }
